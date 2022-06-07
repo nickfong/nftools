77fa3266acbce3b3b220210c44643ef8aba8ad86
Nicholas Fong <nickfong@users.noreply.github.com>
2022-06-07 12:00:00 -0700

    Add option to disable p-value generation in table1

--- R/nf_table1.R
+++ R/nf_table1.R
@@ -6,19 +6,21 @@
 #' @param outcome Outcome to stratify table by
 #' @return A table1 and p-value df
 #' @export
-nf_table1 <- function(columns, outcome, df, minmax=FALSE, reset_p_value_df=FALSE, p_value_df_name='p_value_df') {
-    if((reset_p_value_df) | (!exists('p_value_df'))) {
-        p_value_df <- data.frame()
+nf_table1 <- function(columns, outcome, df, minmax=FALSE, reset_p_value_df=FALSE, p_value_df_name='p_value_df', calculate_pvalue=TRUE) {
+    if (calculate_pvalue) {
+        if((reset_p_value_df) | (!exists('p_value_df'))) {
+            p_value_df <- data.frame()
+        }
+        for (column in columns) {
+            temp <- data.frame(
+                column_name = column,
+                p_value = nftools::nf_pvalue(column, outcome, df)
+            )
+            p_value_df <- rbind(p_value_df, temp)
+        }
+        # Hack to write p_value_df to global environment
+        assign(p_value_df_name, p_value_df, envir=.GlobalEnv)
     }
-    for (column in columns) {
-        temp <- data.frame(
-            column_name = column,
-            p_value = nftools::nf_pvalue(column, outcome, df)
-        )
-        p_value_df <- rbind(p_value_df, temp)
-    }
-    # Hack to write p_value_df to global environment
-    assign(p_value_df_name, p_value_df, envir=.GlobalEnv)
 
     if(minmax) {
         return(table=table1::table1(
